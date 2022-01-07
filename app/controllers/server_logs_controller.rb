d43ecae811dd81ccb6f8463e52e937def35dc826
Dedy Setiawan <dedy.setiawan@sehatq.com>
2022-01-08 06:00:59 +0700

    update filter

--- app/controllers/server_logs_controller.rb
+++ app/controllers/server_logs_controller.rb
@@ -1,15 +1,26 @@
 class ServerLogsController < ApplicationController
   def index
-    @logs = Log.all
+    @logs = Product.where(nil)
+    filtering_params(params).each do |key, value|
+      @logs = @logs.public_send("filter_by_#{key}", value) if value.present?
+    end
   end
 
   def new
       @logs = Log.new
-   end
+  end
 
-   def create
+  def create
      Log.import(params[:log][:file]
      flash[:notice] = "Server Log uploaded successfully"
      redirect_to logs_path #=> or where you want
-   end
+  end
+
+  private
+
+  # A list of the param names that can be used for filtering the Product list
+  def filtering_params(params)
+    params.slice(:ipaddress, :method, :statuscode, :daterange)
+  end
 end
+
--- app/models/log.rb
+++ app/models/log.rb
@@ -1,4 +1,11 @@
 class Log < ApplicationRecord
+
+	scope :filter_by_ipaddress, -> (ip_address) { where ip_address: ip_address }
+ 	scope :filter_by_method, -> (request_method) { where request_method: request_method }
+ 	scope :filter_by_statuscode, -> (status_code) { where status_code: status_code }
+ 	scope :filter_by_daterange, lambda { where("date_created <= ? AND date_created >= ?", :date_start, :date_end) }
+
+
 	def self.import(file)
         CSV.foreach(file.path, headers: true) do |row|
             LogData = Log.new
